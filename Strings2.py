914a14ea450c1d314da6521291862bc74a12aa7d
216049245 <216049245@mycput.ac.za>
2021-11-09 13:03:54 +0200

    Current progress: 027 - Negative Indexing in Strings

--- Strings2.py
+++ Strings2.py
@@ -1,12 +1,36 @@
 # Date: 2021-11-09
-# Time: 08:47
+# Time: 08:47, 12:34
 # Author: Brandon Kruger
-# Ep 026
+# Ep 026,027
 
-#         012345678
+#         01234567801234
 parrot = "Norwegian Blue"
 
 print(parrot)
 
 print(parrot[3])
+print(parrot[4])
+print()
+print(parrot[3])
+print(parrot[6])
+print(parrot[8])
+
+print()
+
+# Negative indexing in Strings (Subtract the length (14) from the positive numbers).
+print(parrot[3 - 14])
+print(parrot[4 - 14])
+print()
+print(parrot[3 - 14])
+print(parrot[6 - 14])
+print(parrot[8 - 14])
+
+# Simply typed as.
+print(parrot[-11])
+print(parrot[-1])
+print()
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
+
 
